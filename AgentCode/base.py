8206ee1e981fe4e8f443541b42f07dc3b9a901d5
Manuel Hoss <manuel.hoss@hotmail.de>
2016-05-13 16:40:39 +0200

    SEL-11 Implemented export to STEP file, removed CSV code and files

--- AgentCode/base.py
+++ AgentCode/base.py
@@ -2,6 +2,9 @@
 import os
 from time import *
 
+import OCC.BRepPrimAPI
+import aocxchange.step_ocaf
+from OCC.gp import gp_Pnt
 from agent import Agent
 
 
@@ -9,32 +12,42 @@ class multiagent(object):
     def __init__(self):
         self.agents = []
 
-        for i in range(2):
+        for i in range(3):
             self.agents.append(Agent())
 
-        for j in range(10):
+        for j in range(3):
             for x in range(50):
-                self.agents[0].doStep()
-            self.__writeToFile__()
+                for agent in self.agents:
+                    agent.doStep()
+            self.__exportToStepFile__()
 
-    def __writeToFile__(self):
-        destinationFolder = os.path.join(os.curdir, "FusionExport")
-        destinationFileName = os.path.join(destinationFolder, strftime("%Y.%m.%d %H-%M-%S.fusionExport", localtime()))
+    def __exportToStepFile__(self):
+        # Create empty export file
+        destinationFolder = os.path.join(os.curdir, "STEPExport")
+        destinationFileName = os.path.join(destinationFolder, strftime("%Y.%m.%d %H-%M-%S.stp", localtime()))
         if not os.path.exists(destinationFolder):
             os.mkdir(destinationFolder)
         while os.path.exists(destinationFileName):
             return
-        f = open(destinationFileName, 'w')
 
+        step_exporter = aocxchange.step_ocaf.StepOcafExporter(destinationFileName)
         for agent in self.agents:
-            f.writelines(
-                str.format("{0};{1};{2};{3}\r", "Agent", agent.position["x"], agent.position["y"], agent.position["z"]))
+            # Create Box at Position agent.position with dimensions 10mm x 10mm x 10mm
+            agent_box_shape = OCC.BRepPrimAPI.BRepPrimAPI_MakeBox(
+                gp_Pnt(agent.position["x"], agent.position["y"], agent.position["z"]), 1, 1, 1).Shape()
+            step_exporter.set_color(r=1, g=0, b=0)  # red
+            step_exporter.set_layer('red')
+            step_exporter.add_shape(agent_box_shape)
 
         for block in self.agents[0].blocks:
-            f.writelines(
-                str.format("{0};{1};{2};{3}\r", "Block", block.position["x"], block.position["y"], block.position["z"]))
+            # Create Box at Position block.position with dimensions 10mm x 10mm x 10mm
+            block_box_shape = OCC.BRepPrimAPI.BRepPrimAPI_MakeBox(
+                gp_Pnt(block.position["x"], block.position["y"], block.position["z"]), 1, 1, 1).Shape()
+            step_exporter.set_color(r=0, g=0, b=0)  # black
+            step_exporter.set_layer('black')
+            step_exporter.add_shape(block_box_shape)
 
-        f.close()
+        step_exporter.write_file()
 
 
 def main():
